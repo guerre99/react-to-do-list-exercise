ad2cd02cda06d9e6fd051c9d30695b56b31b62cd
guerre99 <dgr761@gmail.com>
2023-11-20 17:56:43 +0100

    4ª iteracion

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,30 @@
 import { useState } from 'react'
+import Form from 'react-bootstrap/Form'
+
+const tasks = []
 
 function App() {
+  const [addTask, setAddTask] = useState('')
   return (
     <div className="App">
-      <h1>React to do list</h1>
+      <Form.Control
+        className="task-add"
+        type="text"
+        value={addTask}
+        placeholder="Add TO DO..."
+        onChange={(e) => {
+          setAddTask(e.target.value)
+        }}
+        onKeyDown={(e) => {
+          if (e.code === 'Enter') {
+            tasks.push(addTask)
+            setAddTask('')
+          }
+        }}
+      />
+      {tasks.map((task) => (
+        <ul>{task}</ul>
+      ))}
     </div>
   )
 }
